d04d8be1fc1b83cf984ba78d41dd60cd9fc3f9d6
Josh Henry <josh.d.henry@gmail.com>
2021-10-03 00:51:37 -0700

    Make search bar work

--- ImageSearch/Models/FlickrApiService.swift
+++ ImageSearch/Models/FlickrApiService.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 class FlickrAPIService {
-    class func fetchImages(page: Int, completion: @escaping (_ fetchSuccessful: Bool, _ fetchedImages: [FlickrURLs])->()) {
-        print("Fetching images")
+    class func fetchImages(page: Int, query: String, completion: @escaping (_ fetchSuccessful: Bool, _ fetchedImages: [FlickrURLs])->()) {
+        print("Fetching images: ", query)
         //        let flickrURL = "http://api.flickr.com/services/feeds/photos_public.gne?tags=vegetables&tagmode=any&format=json&jsoncallback=?"
 
         //        let flickrURL = "https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=6343a66eb46c461c91934e8a7a981056&gallery_id=66911286-72157647277042064&format=json&nojsoncallback=1"
                 
-        let flickrURL = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=6343a66eb46c461c91934e8a7a981056&text=flowers&format=json&nojsoncallback=1"
+        let flickrURL = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=6343a66eb46c461c91934e8a7a981056&text=" + query + "&format=json&nojsoncallback=1"
         let session = URLSession.shared
 
         let dataTask = session.dataTask(with: NSURL(string :flickrURL)! as URL) { (data, response, error) in
--- ImageSearch/View Controllers/ImagesViewController.swift
+++ ImageSearch/View Controllers/ImagesViewController.swift
@@ -13,6 +13,7 @@ class ImagesViewController: UIViewController, UISearchBarDelegate {
     @IBOutlet weak var imageSearchBar: UISearchBar!
     
     var imageData = [FlickrURLs]()
+    var query: String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,39 +22,32 @@ class ImagesViewController: UIViewController, UISearchBarDelegate {
         imagesTableView.dataSource = self
         imageSearchBar.delegate = self
         
-        FlickrAPIService.fetchImages(page: 1, completion: {(fetchSuccessful, fetchedImages) in
-            self.imageData = fetchedImages
-            //Update the table view on the main thread
-            DispatchQueue.main.async {
-                self.imagesTableView.reloadData()
-            }
-        })
-        }
-    
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-//        if let text = textField.text {
-//            filterText(text + string)
-//        }
-//
-        return true
-    }
-    
-//    func filterText(_ query: String) {
-//        filteredData.removeAll()
-//        for string in data {
-//            if string.lowercased().starts(with: query.lowercased()) {
-//                filteredData.append(string)
+//        FlickrAPIService.fetchImages(page: 1, completion: {(fetchSuccessful, fetchedImages) in
+//            self.imageData = fetchedImages
+//            //Update the table view on the main thread
+//            DispatchQueue.main.async {
+//                self.imagesTableView.reloadData()
 //            }
-//        }
-//
-//        imagesTableView.reloadData()
-//    }
+//        })
+    }
 
     func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
         print("Did end editing")
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        print("Search button clicked")
+        print("Search button clicked. Search for " + query)
+        
+        FlickrAPIService.fetchImages(page: 1, query: query, completion: {(fetchSuccessful, fetchedImages) in
+            self.imageData = fetchedImages
+            //Update the table view on the main thread
+            DispatchQueue.main.async {
+                self.imagesTableView.reloadData()
+            }
+        })
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        query = searchText
     }
 }
